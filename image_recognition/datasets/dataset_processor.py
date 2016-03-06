924db0fb0af329e19f3c5e8c3d16fc67f9e7d6d9
Thran <thran@centrum.cz>
2016-03-06 19:38:41 +0100

    refactor

--- image_recognition/datasets/dataset_processor.py
+++ image_recognition/datasets/dataset_processor.py
@@ -1,24 +1,27 @@
 import json
-from six.moves import urllib
+import os
+import urllib.request
 
 import scipy.io
 
 
 def procces_102_oxford_dataset():
-    labels = scipy.io.loadmat("datasets/102-Oxford/imagelabels.mat")
+    labels = scipy.io.loadmat("102-Oxford/imagelabels.mat")
     labels = labels["labels"][0]
 
 
 def download_flowerchecker_dataset(size="big"):
     DATA_URL = "http://images.flowerchecker.com/images/{}-{}"
-    FILE_PATH = "datasets/flowerchecker/images/{}.jpg"
+    FILE_PATH = "flowerchecker/images/{}.jpg"
 
-    data = json.load(open("datasets/flowerchecker/images.json"))
+    data = json.load(open("flowerchecker/images.json"))
     for label, d in data.items():
         for i, image in enumerate(d["images"]):
             print('\r>> Downloading FC images of {}: {} from {} - {:.1f}%'
                   .format(label, i + 1, len(d["images"]), (i + 1) / len(d["images"]) * 100.0), end="")
-            urllib.request.urlretrieve(DATA_URL.format(image, size), FILE_PATH.format(image))
+            file_name = FILE_PATH.format(image)
+            if not os.path.exists(file_name):
+                urllib.request.urlretrieve(DATA_URL.format(image, size), file_name)
         print()
 
 download_flowerchecker_dataset()
--- image_recognition/utils.py
+++ image_recognition/utils.py
@@ -1,7 +1,7 @@
 import tarfile
 import os
 import sys
-from six.moves import urllib
+import urllib.request
 
 import tensorflow as tf
 import numpy as np
@@ -22,7 +22,7 @@ def ensure_dir_exists(dir_name):
         os.makedirs(dir_name)
 
 
-def maybe_download_and_extract():
+def maybe_download_inception_and_extract():
     filename = DATA_URL.split('/')[-1]
     dir_name = os.path.join(MODEL_DIR, filename)
     ensure_dir_exists(dir_name)
@@ -86,69 +86,3 @@ def compute_bottlenecks(sess, graph, images, image_names, image_data_placeholder
             np.save(cache_filename, bottleneck_values)
         bottlenecks_values.append(bottleneck_values)
     return np.array(bottlenecks_values)
-
-
-def mnist_train(epochs=1):
-    def evaluate(dataset):
-        images = dataset.images
-        labels = dataset.labels
-        hits = 0
-        for i, (image, label) in enumerate(zip(images, labels)):
-            print('\r>> Evaluation {} from {} - {:.1f}%'.format(i, len(images), i / len(images) * 100.0), end="")
-            label = np.expand_dims(label, 0)
-            image = np.expand_dims(image, 0)
-            image = image.repeat(3, axis=1).reshape(28, 28, 3) * 255
-            hits += sess.run(accuracy, feed_dict={image_data_placeholder: image, ground_truth_placeholder: label})
-        print("\nTest accuracy: {:.3f}%".format(hits / len(images) * 100))
-
-    dir_name = maybe_download_and_extract()
-    graph = load_google_inception_graph(dir_name)
-
-    with tf.Session() as sess:
-        train_step, cross_entropy_mean, accuracy = add_final_training_ops(graph, 10)
-
-        image_data_placeholder = graph.get_tensor_by_name("Cast:0")
-        bottleneck_tensor = graph.get_tensor_by_name(BOTTLENECK_TENSOR_NAME)
-        ground_truth_placeholder = graph.get_tensor_by_name('ground_truth:0')
-
-        sess.run(tf.initialize_all_variables())
-
-        mnist = input_data.read_data_sets("../mnist/MNIST_data/", one_hot=True)
-        iterations = len(mnist.train.labels)
-        for i in range(iterations):
-            print('\r>> Learning {} from {} - {:.1f}%'.format(i, iterations, i / iterations * 100.0), end="")
-            batch_data, batch_labels = mnist.train.next_batch(1)
-            batch_data = batch_data.repeat(3, axis=1).reshape(28, 28, 3) * 255
-            bottlenecks = compute_bottlenecks(sess, graph, images=[batch_data], image_names=[i],
-                                              image_data_placeholder=image_data_placeholder,
-                                              bottleneck_tensor=bottleneck_tensor)
-            train_step.run(feed_dict={bottleneck_tensor: bottlenecks, ground_truth_placeholder: batch_labels})
-        print()
-        evaluate(mnist.test)
-
-
-def panda_test(num_top_predictions=5):
-    dir_name = maybe_download_and_extract()
-    image_filename = os.path.join(dir_name, "cropped_panda.jpg")
-
-    image_data = tf.gfile.FastGFile(image_filename, 'rb').read()
-
-    graph = load_google_inception_graph(dir_name)
-    for op in graph.get_operations():
-        print(op.name)
-
-    with tf.Session() as sess:
-        jpeg_data_placeholder = graph.get_tensor_by_name(JPEG_DATA_TENSOR_NAME)
-        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-        predictions = sess.run(softmax_tensor, {jpeg_data_placeholder: image_data})
-        r = sess.run(graph.get_tensor_by_name("Mul/y:0"), {jpeg_data_placeholder: image_data})
-        print(r, r.dtype, r.shape)
-
-    predictions = np.squeeze(predictions)
-
-    top_k = predictions.argsort()[-num_top_predictions:][::-1]
-    for node_id in top_k:
-        print(node_id, predictions[node_id])
-
-
-mnist_train()
