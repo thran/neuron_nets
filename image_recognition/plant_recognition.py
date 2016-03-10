ea2e0042220cf7ceba954398d1f40644224f3074
Thran <thran@centrum.cz>
2016-03-10 08:53:11 +0100

    image distortions

--- image_recognition/plant_recognition.py
+++ image_recognition/plant_recognition.py
@@ -4,17 +4,21 @@ from image_recognition.utils import *
 
 CACHE_DIR = "cache"
 JPEG_DATA_TENSOR_NAME = 'DecodeJpeg/contents:0'
+RESIZED_INPUT_TENSOR_NAME = 'ResizeBilinear:0'
 BOTTLENECK_TENSOR_NAME = 'pool_3/_reshape:0'
 BOTTLENECK_TENSOR_SIZE = 2048
+INPUT_HEIGHT, INPUT_WIDTH = 299, 299
 
 
 class NetEnd:
     name = "Abstract Net end"
 
-    def __init__(self, learning_rate=1e-4, optimizer=None):
+    def __init__(self, learning_rate=1e-4, optimizer=None, distort=None):
         self.graph = None
         self.class_count = None
         self.image_data_placeholder = None
+        self.resized_image_data_placeholder = None
+        self.distorted_image_tensor = None
         self.bottleneck_tensor = None
         self.predictions = None
         self.keep_prob_placeholder = None
@@ -23,6 +27,7 @@ class NetEnd:
         self.accuracy = None
         self._learning_rate = learning_rate
         self._optimizer = optimizer if optimizer is not None else tf.train.AdamOptimizer
+        self._distort = distort
 
         self.train_step = None
 
@@ -30,11 +35,21 @@ class NetEnd:
         self.graph = graph
         self.prepare_tensors()
         self.class_count = class_count
+        if self._distort is not None:
+            self.distorted_image_tensor = add_input_distortions(
+                self.image_data_placeholder, INPUT_HEIGHT, INPUT_WIDTH, 3, min_crop=self._distort["crop"],
+                random_brightness=self._distort["brightness"], flip_left_right=self._distort["flip"])
         self.add_end()
         self.add_train_step()
 
     def add_end(self):
         pass
+
+    def distort_images(self, sess, images):
+        distorted = []
+        for image in images:
+            distorted.append(sess.run(self.distorted_image_tensor, feed_dict={self.image_data_placeholder: image}))
+        return distorted
 
     def add_train_step(self):
         self.cross_entropy = -tf.reduce_sum(self.ground_truth_placeholder * tf.log(self.predictions))
@@ -45,7 +60,7 @@ class NetEnd:
         correct_prediction = tf.equal(tf.argmax(self.predictions, 1), labels)
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         tf.scalar_summary("accuracy", self.accuracy)
-        tf.histogram_summary("probabilities", self.predictions)
+        # tf.histogram_summary("probabilities", self.predictions)
         tf.scalar_summary("top3", in_top_k(self.predictions, labels, 3))
         tf.scalar_summary("top5", in_top_k(self.predictions, labels, 5))
         tf.scalar_summary("top10", in_top_k(self.predictions, labels, 10))
@@ -55,15 +70,29 @@ class NetEnd:
             raise AttributeError("Model name not specified")
         s = self.name
         (args, _, _, defaults) = inspect.getargspec(self.__init__)
-        s += "".join([", {}:{}".format(a, str(getattr(self, "_" + a))) for a, d in zip(args[-len(defaults):], defaults)
+        args += ("learning_rate", "optimizer", "distort")
+        defaults += (1e-4, tf.train.AdamOptimizer, None)
+        s += "".join([", {}:{}".format(a, repr(getattr(self, "_" + a))) for a, d in zip(args[-len(defaults):], defaults)
                       if getattr(self, "_" + a) != d])
         return s
 
     def prepare_tensors(self):
         self.bottleneck_tensor = self.graph.get_tensor_by_name(BOTTLENECK_TENSOR_NAME)
         self.image_data_placeholder = self.graph.get_tensor_by_name(JPEG_DATA_TENSOR_NAME)
+        self.resized_image_data_placeholder = self.graph.get_tensor_by_name(RESIZED_INPUT_TENSOR_NAME)
         self.keep_prob_placeholder = tf.placeholder("float")
         self.ground_truth_placeholder = tf.placeholder(tf.float32, [None, self.class_count])
+
+    def get_bottlenecks(self, sess, data, evaluation=False):
+        data, labels, identificators = data
+        if self._distort and not evaluation:
+            data = self.distort_images(sess, data)
+            return compute_bottlenecks(sess, data, identificators,
+                                       self.resized_image_data_placeholder, self.bottleneck_tensor)
+
+        cache_dir = CACHE_DIR
+        return compute_bottlenecks(sess, data, identificators,
+                                   self.image_data_placeholder, self.bottleneck_tensor, cache_dir)
 
 
 class SimpleNetEnd(NetEnd):
@@ -112,14 +141,8 @@ class Recognizer:
         self.ne.prepare(self.graph, self.data_set.class_count)
         self.tensor_board_path = os.path.join("/tmp/plant_recognition", str(self.ne))
 
-    def get_bottlenecks(self, sess, data):
-        data, labels, identificators = data
-        cache_dir = CACHE_DIR
-        return compute_bottlenecks(sess, self.graph, data, identificators,
-                                   self.ne.image_data_placeholder, self.ne.bottleneck_tensor, cache_dir)
-
     def evaluate(self, sess, data, summaries=None, print_string=None):
-        test_bottlenecks = self.get_bottlenecks(sess, data)
+        test_bottlenecks = self.ne.get_bottlenecks(sess, data, evaluation=True)
 
         ops = [self.ne.accuracy]
         if summaries is not None:
@@ -135,15 +158,15 @@ class Recognizer:
             return results
         return results[0]
 
-    def train(self, iterations=30000, batch_size=50, evaluate_every=1000):
+    def train(self, iterations=30000, batch_size=50, evaluate_every=200):
         with tf.Session() as sess:
-            writer = tf.train.SummaryWriter(self.tensor_board_path, sess.graph_def)
+            writer = tf.train.SummaryWriter(self.tensor_board_path, sess.graph_def, flush_secs=30)
             summaries = tf.merge_all_summaries()
 
             sess.run(tf.initialize_all_variables())
             for i in range(iterations):
                 batch = self.data_set.train.get_batch(batch_size)
-                bottlenecks = self.get_bottlenecks(sess, batch)
+                bottlenecks = self.ne.get_bottlenecks(sess, batch)
                 self.ne.train_step.run(feed_dict={
                     self.ne.bottleneck_tensor: bottlenecks,
                     self.ne.ground_truth_placeholder: batch[1],
@@ -160,7 +183,13 @@ class Recognizer:
 FC_data_set = FlowerCheckerDataSet()
 FC_data_set.prepare_data()
 
-# ne = SimpleNetEnd()
-ne = HiddenLayersNetEnd([2048], learning_rate=5e-4)
-rec = Recognizer(FC_data_set, ne)
-rec.train()
+if True:
+    ne = HiddenLayersNetEnd([2048], distort={"crop": 0.5, "brightness": 0.3, "flip": True})
+    rec = Recognizer(FC_data_set, ne)
+    print(ne)
+    rec.train(evaluate_every=20)
+else:
+    ne = HiddenLayersNetEnd([2048], learning_rate=0.001)
+    rec = Recognizer(FC_data_set, ne)
+    print(ne)
+    rec.train()
--- image_recognition/utils.py
+++ image_recognition/utils.py
@@ -4,6 +4,7 @@ import urllib.request
 from hashlib import sha1
 import tensorflow as tf
 import numpy as np
+from tensorflow.python.framework import tensor_shape
 
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
 GRAPH_DEF_PB = 'classify_image_graph_def.pb'
@@ -49,22 +50,26 @@ def load_google_inception_graph(dir_name):
     return sess.graph
 
 
-def compute_bottlenecks(sess, graph, data, identificators, feed_placeholder, bottleneck_tensor, cache_dir):
+def compute_bottlenecks(sess, data, identificators, feed_placeholder, bottleneck_tensor, cache_dir=None):
     bottlenecks_values = []
-    cache_dir = os.path.join(cache_dir, "bottlenecks")
-    ensure_dir_exists(cache_dir)
+    if cache_dir is not None:
+        cache_dir = os.path.join(cache_dir, "bottlenecks")
+        ensure_dir_exists(cache_dir)
     iterations = len(data)
     computed = False
     for i, (image, name) in enumerate(zip(data, identificators)):
-        cache_filename = os.path.join(cache_dir, "{}.npy".format(name))
-        if os.path.exists(cache_filename):
-            bottleneck_values = np.load(cache_filename)
-        else:
-            computed = True
-            print('\r>> Computing bottlenecks for {}: {} from {} - {:.1f}%'
-                  .format("data", i + 1, iterations, (i + 1) / iterations * 100.0), end="")
+        if cache_dir is None:
             bottleneck_values = sess.run(bottleneck_tensor, feed_dict={feed_placeholder: image})[0]
-            np.save(cache_filename, bottleneck_values)
+        else:
+            cache_filename = os.path.join(cache_dir, "{}.npy".format(name))
+            if os.path.exists(cache_filename):
+                bottleneck_values = np.load(cache_filename)
+            else:
+                computed = True
+                print('\r>> Computing bottlenecks for {}: {} from {} - {:.1f}%'
+                      .format("data", i + 1, iterations, (i + 1) / iterations * 100.0), end="")
+                bottleneck_values = sess.run(bottleneck_tensor, feed_dict={feed_placeholder: image})[0]
+                np.save(cache_filename, bottleneck_values)
         bottlenecks_values.append(bottleneck_values)
     if computed:
         print()
@@ -81,3 +86,28 @@ def dense_to_one_hot(labels, num_classes):
     labels_one_hot = np.zeros((num_labels, num_classes))
     labels_one_hot.flat[index_offset + labels.ravel()] = 1
     return labels_one_hot
+
+
+def add_input_distortions(image_data_placeholder, input_height, input_width, input_depth,
+                          flip_left_right=False, max_crop=1, min_crop=1, random_brightness=1):
+    decoded_image = tf.image.decode_jpeg(image_data_placeholder)
+    image = tf.cast(decoded_image, dtype=tf.float32)
+    image_4d = tf.expand_dims(image, 0)
+
+    scale_value = tf.random_uniform(tensor_shape.scalar(), minval=1 / max_crop, maxval=1 / min_crop)
+
+    precrop_width, precrop_height = tf.mul(scale_value, input_width), tf.mul(scale_value, input_height)
+    precrop_shape_shape = tf.cast(tf.pack([precrop_height, precrop_width]), dtype=tf.int32)
+    precropped_image = tf.image.resize_bilinear(image_4d, precrop_shape_shape)
+    image_3d = tf.squeeze(precropped_image, squeeze_dims=[0])
+    cropped_image = tf.random_crop(image_3d, [input_height, input_width, input_depth])
+
+    if flip_left_right:
+        flipped_image = tf.image.random_flip_left_right(cropped_image)
+    else:
+        flipped_image = cropped_image
+    brightness_min = 1.0 - random_brightness
+    brightness_max = 1.0 + random_brightness
+    brightness_value = tf.random_uniform(tensor_shape.scalar(), minval=brightness_min, maxval=brightness_max)
+    brightened_image = tf.mul(flipped_image, brightness_value)
+    return tf.expand_dims(brightened_image, 0)
