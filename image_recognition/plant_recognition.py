6cd683c37a61548d78734c17166145a5a18a4d47
Thran <thran@centrum.cz>
2016-03-12 20:51:54 +0100

    checkpoints

--- image_recognition/plant_recognition.py
+++ image_recognition/plant_recognition.py
@@ -2,6 +2,7 @@ import inspect
 from image_recognition.dataset import FlowerCheckerDataSet
 from image_recognition.utils import *
 
+TENSOR_BOARD_DIR = "../tenzor_board"
 CACHE_DIR = "/home/thran/projects/cache"
 JPEG_DATA_TENSOR_NAME = 'DecodeJpeg/contents:0'
 RESIZED_INPUT_TENSOR_NAME = 'ResizeBilinear:0'
@@ -59,7 +60,8 @@ class NetEnd:
         return distorted
 
     def add_train_step(self):
-        self.cross_entropy = -tf.reduce_sum(self.ground_truth_placeholder * tf.log(self.predictions))
+        self.cross_entropy = -tf.reduce_sum(self.ground_truth_placeholder *
+                                            tf.log(tf.clip_by_value(self.predictions, 1e-10, 1.0)))
         cross_entropy_mean = tf.reduce_mean(self.cross_entropy)
         self.train_step = self._optimizer(self._learning_rate).minimize(cross_entropy_mean)
 
@@ -79,11 +81,14 @@ class NetEnd:
         (args, _, _, defaults) = inspect.getargspec(self.__init__)
         if not defaults:
             args, defaults = tuple(), tuple()
-        args += ("learning_rate", "optimizer", "distort", "cut_early")
-        defaults += (1e-4, tf.train.AdamOptimizer, None, False)
+        args += ("distort", "cut_early")
+        defaults += (None, False)
         s += "".join([", {}:{}".format(a, represent(getattr(self, "_" + a))) for a, d in zip(args[-len(defaults):], defaults)
                       if getattr(self, "_" + a) != d])
         return s
+
+    def __repr__(self):
+        return hash_str(str(self))
 
     def prepare_tensors(self):
         self.bottleneck_tensor = self.graph.get_tensor_by_name(BOTTLENECK_TENSOR_NAME)
@@ -97,8 +102,11 @@ class NetEnd:
         self.bottleneck_tensor_size = 1
         for s in cut_tensor.get_shape():
             self.bottleneck_tensor_size *= int(s)
-        self.bottleneck_tensor = tf.reshape(cut_tensor, (1, self.bottleneck_tensor_size))
-        self.cache_dir = os.path.join(CACHE_DIR, EARLY_CUT)
+
+        max_pool = tf.nn.max_pool(cut_tensor, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        self.bottleneck_tensor_size //= 4
+        self.bottleneck_tensor = tf.reshape(max_pool, (1, self.bottleneck_tensor_size))
+        self.cache_dir = os.path.join(CACHE_DIR, EARLY_CUT.replace('/', "#"))
 
     def get_bottlenecks(self, sess, data, evaluation=False, epoch=0):
         data, labels, identificators = data
@@ -158,7 +166,8 @@ class Recognizer:
         self.graph = load_google_inception_graph(model_dir_name)
         self.ne = net_end
         self.ne.prepare(self.graph, self.data_set.class_count)
-        self.tensor_board_path = os.path.join("/tmp/plant_recognition", str(self.ne))
+        self.tensor_board_path = os.path.join(TENSOR_BOARD_DIR, str(self.ne))
+        self.save_path = os.path.join(CACHE_DIR, "checkpoints", repr(self.ne))
 
     def evaluate(self, sess, data, summaries=None, print_string=None):
         test_bottlenecks = self.ne.get_bottlenecks(sess, data, evaluation=True)
@@ -177,13 +186,16 @@ class Recognizer:
             return results
         return results[0]
 
-    def train(self, iterations=30000, batch_size=50, evaluate_every=200):
+    def train(self, batch_size=50, evaluate_every=200, save_every=5000, checkpoint=None):
         with tf.Session() as sess:
             writer = tf.train.SummaryWriter(self.tensor_board_path, sess.graph_def, flush_secs=30)
             summaries = tf.merge_all_summaries()
-
-            sess.run(tf.initialize_all_variables())
-            for i in range(iterations):
+            saver = tf.train.Saver()
+            i = self.load_last_checkpoint(sess, checkpoint, saver)
+            if i == 0:
+                sess.run(tf.initialize_all_variables())
+            while True:
+                i += 1
                 batch = self.data_set.train.get_batch(batch_size)
                 bottlenecks = self.ne.get_bottlenecks(sess, batch, epoch=self.data_set.train.finished_epochs)
                 self.ne.train_step.run(feed_dict={
@@ -195,21 +207,39 @@ class Recognizer:
                 if i % evaluate_every == 0:
                     accuracy, summary_str = self.evaluate(sess, self.data_set.validation.get_all(), summaries=summaries)
                     writer.add_summary(summary_str, i * batch_size)
-                    print("\r>>> Step: {}/{}, validation accuracy: {:.2f}; epochs: {}".format(
-                        i, iterations, accuracy * 100, self.data_set.train.finished_epochs), end="")
+                    print("\r>>> Step: {}, validation accuracy: {:.2f}; epochs: {}".format(
+                        i, accuracy * 100, self.data_set.train.finished_epochs), end="")
+
+                if (i + 1) % save_every == 0:
+                    path = os.path.join(self.save_path, "checkpoint")
+                    ensure_dir_exists(path  )
+                    saver.save(sess, path, global_step=i + 1)
+
+    def load_last_checkpoint(self, sess, checkpoint=None, saver=None):
+        saver = saver if saver else tf.train.Saver()
+        last_checkpoint = checkpoint if checkpoint else tf.train.latest_checkpoint(self.save_path)
+        if last_checkpoint:
+            saver.restore(sess, last_checkpoint)
+            i = int(last_checkpoint.split("-")[-1])
+        else:
+            i = 0
+        return i
 
 
 FC_data_set = FlowerCheckerDataSet()
 FC_data_set.prepare_data()
 
 if False:
-    ne = HiddenLayersNetEnd([2048], distort={"crop": 0.5, "brightness": 0.3, "flip": True, "epochs": 10})
+    ne = SimpleNetEnd()
+    # ne = HiddenLayersNetEnd([2048], distort={"crop": 0.5, "brightness": 0.3, "flip": True, "epochs": 10})
     rec = Recognizer(FC_data_set, ne)
     print(ne)
-    rec.train(evaluate_every=50)
+    rec.train(evaluate_every=50, save_every=100)
+
 if True:
     # ne = SimpleNetEnd(cut_early=True)
-    ne = HiddenLayersNetEnd([2048], cut_early=True)
+    ne = HiddenLayersNetEnd([2048], learning_rate=1e-4,
+                            distort={"crop": 0.5, "brightness": 0.3, "flip": True, "epochs": 10}, cut_early=True)
     rec = Recognizer(FC_data_set, ne)
-    print(ne)
+    print(ne, repr(ne))
     rec.train()
