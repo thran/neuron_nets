157b71150a918a1dd604c66979741718eaf2169f
Thran <thran@centrum.cz>
2016-03-27 21:23:49 +0200

    download new data

--- image_recognition/datasets/dataset_processor.py
+++ image_recognition/datasets/dataset_processor.py
@@ -1,6 +1,7 @@
 import json
 import os
 import urllib.request
+import urllib.error
 
 import scipy.io
 
@@ -10,18 +11,24 @@ def procces_102_oxford_dataset():
     labels = labels["labels"][0]
 
 
-def download_flowerchecker_dataset(size="big"):
+def download_flowerchecker_dataset(dataset_file="flowerchecker/images.json", size="big"):
     DATA_URL = "http://images.flowerchecker.com/images/{}-{}"
     FILE_PATH = "flowerchecker/images/{}.jpg"
 
-    data = json.load(open("flowerchecker/images.json"))
-    for label, d in data.items():
-        for i, image in enumerate(d["images"]):
+    data = json.load(open(dataset_file))
+    for label in sorted(data.keys()):
+        d = data[label]
+        images = d["images"] if "images" in d else [p["image"] for p in d]
+        for i, image in enumerate(images):
+            count = len(images)
             print('\r>> Downloading FC images of {}: {} from {} - {:.1f}%'
-                  .format(label, i + 1, len(d["images"]), (i + 1) / len(d["images"]) * 100.0), end="")
+                  .format(label, i + 1, count, (i + 1) / count * 100.0), end="")
             file_name = FILE_PATH.format(image)
             if not os.path.exists(file_name):
-                urllib.request.urlretrieve(DATA_URL.format(image, size), file_name)
+                try:
+                    urllib.request.urlretrieve(DATA_URL.format(image, size), file_name)
+                except urllib.error.HTTPError:
+                    print("Problem with image", image)
         print()
 
-download_flowerchecker_dataset()
+# download_flowerchecker_dataset("flowerchecker/dataset.json")
